b39b86a0b49a8a5f504f291e4abfd18198fd9ad6
Sara <sara.yohannes94@gmail.com>
2026-04-08 13:17:28 -0400

    Remove SSR hydration for Vercel deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,11 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient()
-    ]
+  ]
 };
